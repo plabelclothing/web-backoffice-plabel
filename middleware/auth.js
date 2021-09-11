531974f00974193da6dbdce2c40be29eb889138f
Alex <alex.ziabko@paymento.pl>
2021-09-11 14:38:55 +0200

    updated auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,16 +4,14 @@ import {CookieConst, RouteConst} from 'assets/const';
 export default async ({app, route, redirect}) => {
 	try {
 		const cookie = app.$cookies.get(CookieConst.A_COOKIE);
-		const arrCheckRoute = [RouteConst.AUTHORIZATION];
+		const checkRoute = RouteConst.AUTHORIZATION;
 		const currentRoute = route.path.split('/');
-		for (const val of currentRoute) {
-			// if (!cookie && !arrCheckRoute.includes(val)) {
-			// 	console.log(val);
-			// 	return redirect(`/${RouteConst.AUTHORIZATION}`);
-			// }
-			if (cookie && arrCheckRoute.includes(val)) {
-				return redirect(`/`);
-			}
+		const isInclude = currentRoute.includes(checkRoute);
+		if (cookie && isInclude) {
+			return redirect(`/`);
+		}
+		if (!cookie && !isInclude) {
+			return redirect(`/${RouteConst.AUTHORIZATION}`);
 		}
 	} catch (e) {
 	}
